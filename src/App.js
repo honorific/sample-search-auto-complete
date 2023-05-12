6fc7f90656c3a2ba43cb7feff7aa738a6a13eeea
honorific <honorific.info@gmail.com>
2023-05-12 19:05:10 +0330

    get users from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg'
 import './App.css'
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get('https://reqres.in/api/users')
+      setUsers(response.data.data)
+    }
+    loadUsers()
+  }, [])
+
   return (
     <div className='App'>
       <input type='text' />
