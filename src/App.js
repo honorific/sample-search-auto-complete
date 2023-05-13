cb6b538ee1fe765e167f3788702f1f846c79162b
honorific <honorific.info@gmail.com>
2023-05-13 10:29:12 +0330

    show reults

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {useEffect, useState} from 'react'
 function App() {
   const [users, setUsers] = useState([])
   const [text, setText] = useState('')
+  const [suggestions, setSuggestions] = useState([])
   useEffect(() => {
     const loadUsers = async () => {
       const response = await axios.get('https://reqres.in/api/users')
@@ -15,6 +16,15 @@ function App() {
   }, [])
 
   const changeHandler = (text) => {
+    let matches = []
+    if (text.length > 0) {
+      matches = users.filter((user) => {
+        const regex = new RegExp(`${text}`, 'gi')
+        return user.email.match(regex)
+      })
+    }
+    console.log('matches', matches)
+    setSuggestions(matches)
     setText(text)
   }
 
@@ -27,6 +37,14 @@ function App() {
         onChange={(e) => changeHandler(e.target.value)}
         value={text}
       />
+      {suggestions &&
+        suggestions.map((suggestion, i) => {
+          return (
+            <div key={i} className='col-md-12 justify-content-md-center'>
+              {suggestion.email}
+            </div>
+          )
+        })}
     </div>
   )
 }
